c89c3184c0cc9ebba8c26923389c4df670efd96e
marcin-knapik <marcin.knapik@hotmail.com>
2026-05-22 15:31:28 +0200

    WIP

--- src/HelloWorld.Api/HelloWorldMessage.cs
+++ src/HelloWorld.Api/HelloWorldMessage.cs
@@ -2,5 +2,5 @@ namespace HelloWorld.Api;
 
 public static class HelloWorldMessage
 {
-    public const string Value = "Hello World from Azure App Service!";
+    public const string Value = "Hello World from Azure App Service!2";
 }
