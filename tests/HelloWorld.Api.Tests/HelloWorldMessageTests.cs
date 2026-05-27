1dff439dc1278b91b68a32dbc57f4cf0eea92016
marcin-knapik <marcin.knapik@hotmail.com>
2026-05-27 10:51:26 +0200

    WIP23

--- tests/HelloWorld.Api.Tests/HelloWorldMessageTests.cs
+++ tests/HelloWorld.Api.Tests/HelloWorldMessageTests.cs
@@ -7,6 +7,6 @@ public class HelloWorldMessageTests
     [Fact]
     public void Value_ReturnsExpectedGreeting()
     {
-        Assert.Equal("Hello World from Azure App Service from ...", HelloWorldMessage.Value);
+        Assert.Equal("Hello World from Azure App Service from ...1", HelloWorldMessage.Value);
     }
 }
